6c789a06a4763a239338bdb769d4aeb4fdb54714
guomin <yushi5344@gmail.com>
2017-10-31 22:38:34 +0800

    微信绑定

---
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    //
+    const TOKEN='guomin';
+    public function __construct()
+    {
+
+        $this->check();
+    }
+
+
+    private function check(){
+        if(isset($_GET["signature"])&&isset($_GET["timestamp"])&&isset($_GET["nonce"])&&isset($_GET["echostr"])){
+            $signature = $_GET["signature"];//从用户端获取签名赋予变量signature
+            $timestamp = $_GET["timestamp"];//从用户端获取时间戳赋予变量timestamp
+            $nonce = $_GET["nonce"];    //从用户端获取随机数赋予变量nonce
+
+            $token = self::TOKEN;//将常量token赋予变量token
+            $tmpArr = array($token, $timestamp, $nonce);//简历数组变量tmpArr
+            sort($tmpArr, SORT_STRING);//新建排序
+            $tmpStr = implode( $tmpArr );//字典排序
+            $tmpStr = sha1( $tmpStr );//shal加密
+            //tmpStr与signature值相同，返回真，否则返回假
+            if( $tmpStr == $signature ){
+                echo $_GET["echostr"];;
+            }else{
+                return false;
+            }
+        }
+    }
+}
