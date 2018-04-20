002d6cdb54d36ba8453e115978ca74af75bf63ee
guomin <yushi5344@gmail.com>
2018-04-20 14:20:51 +0800

    excel类

---
+++ app/Http/Controllers/Excel/ExcelController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Excel;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ExcelController extends Controller
+{
+    //
+}
